7a6e9b8c1b0dc1f364351e6ed2bee899e9578742
Noah Kandel <nkandel@umd.edu>
2023-04-04 21:03:26 -0400

    added ComputerPlayer Class

--- sample_game.py
+++ sample_game.py
@@ -1,1 +1,10 @@
 """ a turn based fighter game"""
+
+class HumanPlayer:
+    """The user playing the game
+    
+    Attributes:
+    name (str): the person's name.
+    roster (open_file): roster of fighters user can choose from"""
+    
+    
