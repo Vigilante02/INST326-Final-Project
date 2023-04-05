f6b4c88edbc8118e1ce4f24b6db73be779e59def
Ross Zaslavsky <rzaslavs@terpmail.umd.edu>
2023-04-04 20:44:09 -0400

    added sample_game.py

---
+++ sample_game.py
@@ -0,0 +1,10 @@
+"""A turn-based fighting game"""
+
+class HumanPlayer:
+    """The user playing the game
+    
+    Attributes:
+    - name(str): The name of the player
+    - roster(file): The roster of fighters the player can choose from
+    
+    """
