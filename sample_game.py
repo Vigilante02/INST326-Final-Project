9ce5af229913fbb4e3994a3f4c689f27e4cde100
Ryan Borak <rcborak@gmail.com>
2023-04-04 20:58:10 -0400

    added damage class

--- sample_game.py
+++ sample_game.py
@@ -8,3 +8,12 @@ class HumanPlayer:
     - roster(file): The roster of fighters the player can choose from
     
     """
+    
+class Damage:
+    """
+    Creates a Damage object to calculate the equation used to calculate damage.
+    
+    Attributes:
+    attack (int): Attack power of a character
+    defense (int): Defense power of a character 
+    """
