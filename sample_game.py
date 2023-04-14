e3d2fee6d933926baba42e5cf7b759a49719aca3
Ryan Borak <rcborak@gmail.com>
2023-04-14 09:33:25 -0400

    Added damage class

--- sample_game.py
+++ sample_game.py
@@ -94,3 +94,14 @@ class Final:
     main()
     roster()
     begin()
+
+class Damage: 
+    
+    def __init__(self, attack, defense):
+        self.attack = attack
+        self.defense = defense
+        
+    def calc_dmg(self):
+        dmg = self.attack - (0.5 * self.defense)
+        return dmg
+        
