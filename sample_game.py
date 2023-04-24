bdf04380d797fa5bdb7e50c05ed5eaf74e76b182
nkandel1 <Noahthek@gmail.com>
2023-04-24 09:46:46 -0400

    Fixed begin and you_begin methods

--- sample_game.py
+++ sample_game.py
@@ -17,9 +17,9 @@ class Final:
 
         print("\nHow to play.\n\nPlayers take turns choosing a move. Moves can either be heal or attack")
         print("\nEach player starts with 100 health, and the first to get their opponent to 0 wins!")
-        play_more = True
+        self.play_more = True
 
-        while play_more:
+        while self.play_more:
             winner = None
             self.player_hp = 100
             self.computer_hp = 100
@@ -37,7 +37,7 @@ class Final:
             print("\nPlayer health: ", self.player_hp, "Computer health: ", self.computer_hp)
             print("\nBut first PICK YOUR CHARACTER!!!!")
         
-            play_more = False
+            self.play_more = False
         
     def roster(self):   
         fighter_file = open('fighters.txt', 'r')
@@ -61,33 +61,33 @@ class Final:
         heal_cpu = self.cpu_powers['heal_power']
     def begin(self):
         cpu_move = random.choice(["attack", "heal"])
+        self.cpu_attack_power = self.fighters[self.computer_fighter]['attack_power']
+        self.damage =int(self.cpu_attack_power)
+        self.cpu_heal_power = self.fighters[self.computer_fighter]['heal_power']
+        self.cpu_save = int(self.cpu_heal_power)
         if self.computer_turn == True and cpu_move == "attack":
-            attack_power = self.cpu_powers['attack_power']
-            damage =int(attack_power)
-            self.player_hp -= damage
-            print(f"\n{self.computer_fighter} attacks the player for {damage} damage!")
+            self.player_hp -= self.damage
+            print(f"\n{self.computer_fighter} attacks the player for {self.damage} damage!")
         
         elif self.computer_turn == True and cpu_move == "heal":
-            heal_power = self.fighters[self.computer_fighter]['heal_power']
-            save = self.cpu_powers[heal_power]
-            self.computer_hp += save
-            print(f"\n{self.computer_fighter} heals themself for {save} health!")
+            self.computer_hp += self.cpu_save
+            print(f"\n{self.computer_fighter} heals themself for {self.cpu_save} health!")
             
         print(f"\nPlayer health: {self.player_hp}, Computer health: {self.computer_hp}")
     
     def you_begin(self):
-        player_move = input("\nSelect your move: Attack, deal {attack_power} to the opponent, \nHeal, add to your health by {heal_power}")
+        self.attack_power = self.fighters[self.selected_fighter]['attack_power']
+        self.damage =int(self.attack_power)
+        self.heal_power = self.fighters[self.selected_fighter]['heal_power']
+        self.save = int(self.heal_power)
+        player_move = input(f"\nSelect your move: Attack, deal {self.attack_power} to the opponent, \nHeal, add to your health by {self.heal_power}: ")
         if self.player_turn == True and player_move == "attack":
-            attack_power = self.fighters[self.selected_fighter]['attack_power']
-            damage =int(attack_power)
-            self.computer_hp -= damage
-            print(f"\n{self.selected_fighter} attacks the computer for {damage} damage!")
+            self.computer_hp -= self.damage
+            print(f"\n{self.selected_fighter} attacks the computer for {self.damage} damage!")
         
         elif self.player_turn == True and player_move == "heal":
-            heal_power = self.fighters[self.selected_fighter]['heal_power']
-            save = self.selected_fighter[heal_power]
-            self.player_hp += save
-            print(f"\n{self.selected_fighter} heals themself for {save} health!")
+            self.player_hp += self.save
+            print(f"\n{self.selected_fighter} heals themself for {self.save} health!")
             
         print(f"\nPlayer health: {self.player_hp}, Computer health: {self.computer_hp}")
     def Match(self):
