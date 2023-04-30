7aa54dd150252771299298079383054953ec4ffa
zaslavskyross <rzaslavs@terpmail.umd.edu>
2023-04-30 13:58:00 -0400

    Post AMP Meeting Changes

--- sample_game.py
+++ sample_game.py
@@ -1,93 +1,65 @@
-import json
 import random
 
 class Final:
-    def __init__(self):
-        self.welcome()
-        self.roster()
-        self.begin()
-        self.you_begin()
-        self.Match()
-        
-
+    def __init__(self, player_name, player_hp=100, computer_hp=100):
+        self.player_name = player_name
+        self.player_hp = player_hp
+        self.computer_hp = computer_hp
+        self.fighters = {}
+        self.selected_fighter = ""
+        self.cpu_fighter = ""
+        self.cpu_powers = {}
+        self.player_turn = False
+        self.computer_turn = False
+        self.play_more = True
+        self.winner = None
+    
+    def __eq__(self, other):
+        return (isinstance(other, Final)
+                and self.player_name == other.player_name
+                and self.player_hp == other.player_hp
+                and self.computer_hp == other.computer_hp
+                and self.fighters == other.fighters
+                and self.selected_fighter == other.selected_fighter
+                and self.cpu_fighter == other.cpu_fighter
+                and self.cpu_powers == other.cpu_powers
+                and self.player_turn == other.player_turn
+                and self.computer_turn == other.computer_turn
+                and self.play_more == other.play_more
+                and self.winner == other.winner)
+    
+    def __str__(self):
+        return f"Player: {self.player_name} | Player HP: {self.player_hp} | Computer HP: {self.computer_hp}"
+    
+    def __repr__(self):
+        return f"Final(player_name='{self.player_name}', player_hp={self.player_hp}, computer_hp={self.computer_hp})"
+    
     def welcome(self):
         "Function that welcomes player and explains rules"
-        print("\tWelcome to our Fighter Game by Ross, Ryan and Noah! This is a turn based battle game where")
+        print("\tWelcome to Showup, Showout, Showdown! This is a turn based battle game where")
         print("\tthere can only be one winner!")
 
-        print("\nHow to play.\n\nPlayers take turns choosing a move. Moves can either be heal or attack")
+        print("\nHow to play.\n\nPlayers take turns choosing a move. Moves can either be heal or attack.")
         print("\nEach player starts with 100 health, and the first to get their opponent to 0 wins!")
-        self.play_more = True
 
-        while self.play_more:
-            winner = None
-            self.player_hp = 100
-            self.computer_hp = 100
-            
-            turn = random.randint(1,2)
-            if turn == 1:
-                self.player_turn = True
-                self.computer_turn = False
-                print("\nYou will go first.")
-            else:
-                self.player_turn = False
-                self.computer_turn = True
-                print("\nComputer will go first.")
-            
-            print("\nPlayer health: ", self.player_hp, "Computer health: ", self.computer_hp)
-            print("\nBut first PICK YOUR CHARACTER!!!!")
-        
-            self.play_more = False
-        
-    def roster(self):   
-        fighter_file = open('fighters.txt', 'r')
-        self.fighters = {}
-        for line in fighter_file.readlines():
-            fighter, attack_power, heal_power = line.strip().split(',')
-            self.fighters[fighter] = f'Attack Power: {int(attack_power)} Heal Power: {int(heal_power)}'
+    def load_fighters(self):
+        with open('fighters.txt', 'r', encoding='utf-8') as fighter_file:
+            for line in fighter_file.readlines():
+                fighter, attack_power, heal_power = line.strip().split(',')
+                self.fighters[fighter] = {"attack_power": int(attack_power), "heal_power": int(heal_power)}
 
         fighter_file.close()
-        
+
+    def select_fighter(self):
         print(self.fighters)
-        self.selected_fighter = input("Please enter the name of your fighter: ")
-        if self.selected_fighter in self.fighters:
-            print(self.fighters[self.selected_fighter])
-        else:
-            print("Invalid fighter name.")
-        self.computer_fighter = random.choice(list(self.fighters.keys()))
-        print(f"The computer has selected {self.computer_fighter}.")
-        self.cpu_powers = self.fighters[self.computer_fighter]
-    def begin(self):
-        cpu_move = random.choice(["attack", "heal"])
-        self.cpu_attack_power = int(self.fighters[self.computer_fighter].split()[2])
-        self.damage =int(self.cpu_attack_power)
-        self.cpu_heal_power = int(self.fighters[self.computer_fighter].split()[-1])
-        self.cpu_save = int(self.cpu_heal_power)
-        if self.computer_turn == True and cpu_move == "attack":
-            self.player_hp -= self.damage
-            print(f"\n{self.computer_fighter} attacks the player for {self.damage} damage!")
-        
-        elif self.computer_turn == True and cpu_move == "heal":
-            self.computer_hp += self.cpu_save
-            print(f"\n{self.computer_fighter} heals themself for {self.cpu_save} health!")
-            
-        print(f"\nPlayer health: {self.player_hp}, Computer health: {self.computer_hp}")
-    
-    def you_begin(self):
-        self.attack_power = int(self.fighters[self.selected_fighter].split()[2])
-        self.damage =int(self.attack_power)
-        self.heal_power = int(self.fighters[self.selected_fighter].split()[-1])
-        self.save = int(self.heal_power)
-        player_move = input(f"\nSelect your move: Attack: deal {self.attack_power} to the opponent, \nHeal: add to your health by {self.heal_power}: ")
-        if self.player_turn == True and player_move == "attack":
-            self.computer_hp -= self.damage
-            print(f"\n{self.selected_fighter} attacks the computer for {self.damage} damage!")
-        
-        elif self.player_turn == True and player_move == "heal":
-            self.player_hp += self.save
-            print(f"\n{self.selected_fighter} heals themself for {self.save} health!")
-            
-        print(f"\nPlayer health: {self.player_hp}, Computer health: {self.computer_hp}")
+        while self.selected_fighter not in self.fighters:
+            self.selected_fighter = input("Please enter the name of your fighter: ")
+            if self.selected_fighter not in self.fighters:
+                print("Invalid fighter name.")
+        print(f"{self.fighters[self.selected_fighter]}")
+        self.cpu_fighter = random.choice(list(self.fighters.keys()))
+        print(f"The computer has selected {self.cpu_fighter}.")
+        self.cpu_powers = self.fighters[self.cpu_fighter]
     def Match(self):
         if self.player_hp <= 0:
             self.winner = self.computer_fighter
